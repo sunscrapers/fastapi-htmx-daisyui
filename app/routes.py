7202093716e4ed75ba384e3d21c34b7ae0bc2d54
Patryk Młynarek <p.mlynarek@sunscrapers.com>
2024-01-23 22:56:46 +0100

    Add home view.

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,14 @@
 from fastapi import APIRouter
-from fastapi import status
-from fastapi.responses import RedirectResponse
 
 from app.views.funfact import router as funfact_router
 from app.views.health import router as health_router
+from app.views.home import router as home_router
 from app.views.todo import router as todo_router
 
 main_router = APIRouter()
 
 
-@main_router.get("/")
-async def redirect_todo():
-    return RedirectResponse(url="/todo", status_code=status.HTTP_302_FOUND)
-
-
 main_router.include_router(health_router)
+main_router.include_router(home_router)
 main_router.include_router(todo_router)
 main_router.include_router(funfact_router)
